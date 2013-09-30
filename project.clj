14513a062a2a7180c0ffd4d1b6fd4c774fceb783
Andrey Antukh <niwi@niwi.be>
2013-09-30 08:37:34 +0200

    Remove core.async dependency.

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.3"]
-                 [com.novemberain/langohr "1.5.0"]
-                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]]
+                 [com.novemberain/langohr "1.5.0"]]
 
                  ;; [com.taoensso/nippy "2.1.0"]
                  ;; [com.taoensso/carmine "2.2.1"]
