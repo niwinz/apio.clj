9f440b4d723c2795ad37bfc54e74b791fa907a65
Andrey Antukh <niwi@niwi.be>
2013-09-30 08:37:48 +0200

    Minor name changes.

--- src/apio/cli.clj
+++ src/apio/cli.clj
@@ -1,30 +1,31 @@
 (ns apio.cli
   (:import (java.lang Thread))
   (:require [apio.core :as core]
-            [apio.concurrency.threading :as cc-threading]
-            [apio.concurrency.queue :as cc-queue]
-            [apio.concurrency.util :as cc-util])
+            [apio.concurrency.threading :as cthreading]
+            [apio.concurrency.queue :as cqueue]
+            [apio.concurrency.util :as cutil])
   (:gen-class))
 
 (defn dispatcher
   [queue]
-  (let [pool (cc-threading/fixed-pool (cc-util/max-workers core/*config*))]
+  (let [pool (cthreading/start-pool (cutil/max-workers core/*config*))]
     (loop []
-      (let [task (cc-queue/rcv queue)]
+      (let [task (cqueue/rcv queue)]
         (when (instance? java.util.concurrent.Callable task)
-          (do (cc-threading/spawn pool task) (recur)))))
-    (cc-threading/shutdown-pool pool)))
+          (do (cthreading/spawn pool task) (recur)))))
+    (cthreading/shutdown-pool pool)))
 
 (defn -main
   [path]
-  (let [queue   (cc-queue/queue (cc-util/max-prefetch core/*config*))
-        thr     (cc-threading/thread #(dispatcher queue))
+  (let [queue   (cqueue/queue (cutil/max-prefetch core/*config*))
+        thr     (cthreading/thread #(dispatcher queue))
         tasks   (map (fn [n]
                        (fn []
                          (println "Thread:" (.getId (Thread/currentThread)) "Processing:" n )
-                         (cc-util/sleep (rand-int 1000)) 2)) (range 10))]
+                         (cutil/sleep (rand-int 1000)) 2)) (range 10))]
     (doseq [t tasks]
-      (cc-queue/snd queue t))
+      (cqueue/snd queue t))
 
-    (cc-queue/snd queue 1)
+    ;; Send non callable value to shutdown dispatcher.
+    (cqueue/snd queue 1)
     (.join thr)))
